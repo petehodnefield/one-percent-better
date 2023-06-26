3127a8cf1f146250ae4bffc1452347a0e78214e5
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2023-06-26 09:18:22 -0500

    Added concurrently to root

--- backend/routes/user-routes.js
+++ backend/routes/user-routes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 // Import model
 import User from "../models/User.js";
 
+// Create User
 router.post("/", async (req, res) => {
-  console.log(`Request body ${req.body}`);
   const newPerson = new User({
     name: req.body.name,
   });
@@ -13,9 +13,25 @@ router.post("/", async (req, res) => {
   return res.status(201).json(insertedUser);
 });
 
+// Get all Users
 router.get("/", async (req, res) => {
   const allUsers = await User.find();
   return res.status(200).json(allUsers);
+});
+
+// Update a User
+router.put(":id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findByIdAndUpdate(id, req.body);
+    if (!user) {
+      return res
+        .status(404)
+        .json({ message: `Cannot find nay product with ID ${id}` });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 export default router;
