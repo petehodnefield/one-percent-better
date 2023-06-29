792f5cd3c7098712b6325dde752da2b97e075867
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2023-06-29 09:34:28 -0500

    Delete route succesfully removing improvements fe

--- src/app/components/ImprovementDetails.js
+++ src/app/components/ImprovementDetails.js
@@ -1,7 +1,22 @@
 import React from "react";
 
 const ImprovementDetails = ({ improvement }) => {
-  console.log(improvement);
+  const deleteImprovement = async (improvementID) => {
+    const response = await fetch(
+      `http://localhost:3001/improvement/improvement?id=${improvementID}`,
+      {
+        method: "DELETE",
+        mode: "cors",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const jsonData = await response.json();
+    window.location.reload();
+  };
   return (
     <div className="list__details-wrapper">
       {/* Date */}
@@ -9,7 +24,12 @@ const ImprovementDetails = ({ improvement }) => {
         <p className="list__date">{improvement.date}</p>
         <div className="list__action-wrapper">
           <p className="list__action">Edit</p>
-          <p className="list__action list__action--red">Delete</p>
+          <p
+            className="list__action list__action--red"
+            onClick={() => deleteImprovement(improvement._id)}
+          >
+            Delete
+          </p>
         </div>
       </div>
 
