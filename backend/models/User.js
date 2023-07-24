8a277bca30b7a363360fed2dada2601bdd5b21e6
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2023-07-24 10:50:48 -0500

    Added bcrypt to password

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 const { Schema } = mongoose;
+import bcrypt from "bcrypt";
+const saltRounds = 10;
 
 const userSchema = new mongoose.Schema({
   username: {
@@ -16,33 +18,33 @@ const userSchema = new mongoose.Schema({
   ],
 });
 
-// userSchema.pre("save", function (next) {
-//   const user = this;
+userSchema.pre("save", function (next) {
+  const user = this;
 
-//   // only hash the password if it has been modified (or is new)
-//   if (!user.isModified("password")) return next();
+  // only hash the password if it has been modified (or is new)
+  if (!user.isModified("password")) return next();
 
-//   // generate a salt
-//   bcrypt.genSalt(saltRounds, function (err, salt) {
-//     if (err) return next(err);
+  // generate a salt
+  bcrypt.genSalt(saltRounds, function (err, salt) {
+    if (err) return next(err);
 
-//     // hash the password using our new salt
-//     bcrypt.hash(user.password, salt, function (err, hash) {
-//       if (err) return next(err);
+    // hash the password using our new salt
+    bcrypt.hash(user.password, salt, function (err, hash) {
+      if (err) return next(err);
 
-//       // override the cleartext password with the hashed one
-//       user.password = hash;
-//       next();
-//     });
-//   });
-// });
+      // override the cleartext password with the hashed one
+      user.password = hash;
+      next();
+    });
+  });
+});
 
-// userSchema.methods.comparePassword = function (candidatePassword, cb) {
-//   bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
-//     if (err) return cb(err);
-//     cb(null, isMatch);
-//   });
-// };
+userSchema.methods.comparePassword = function (candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
 
 const User = mongoose.model("User", userSchema);
 
